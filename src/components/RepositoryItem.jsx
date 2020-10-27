23acad4577cad88eaaa7eefef94ac6ce91ca72c6
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-28 07:01:27 +0800

    Update the style of RepositoryList

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,14 +1,117 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
+import Text from "./Text";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 15,
+    backgroundColor: theme.colors.bright.white,
+    borderBottomWidth: 2,
+    borderStyle: "solid",
+    borderColor: theme.colors.bright[3],
+  },
+  upperContainer: {
+    paddingTop: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  avatarContainer: {},
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+  },
+  basicInfo: {
+    marginLeft: 15,
+    flexShrink: 1,
+  },
+  fullName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  descriptionWrapper: {
+    marginTop: 5,
+  },
+  languageWrapper: {
+    marginTop: 5,
+    flexDirection: "row",
+  },
+  languageButton: {
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: theme.colors.frost[4],
+  },
+  language: {
+    color: theme.colors.bright.white,
+    fontSize: 12,
+  },
+  lowerContainer: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  additionalInfo: {
+    justifyContent: "center",
+  },
+  centeredText: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  additionalInfoLabel: {
+    color: theme.colors.dark[1],
+  },
+});
+
+const AdditionalInfo = ({ label, value }) => {
+  const formattedValue =
+    value > 1000 ? `${Math.round(value / 100) / 10}k` : value;
+
+  return (
+    <View style={styles.additionalInfo}>
+      <View style={styles.centeredText}>
+        <Text>{formattedValue}</Text>
+      </View>
+      <View style={styles.centeredText}>
+        <Text style={styles.additionalInfoLabel}>{label}</Text>
+      </View>
+    </View>
+  );
+};
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      {Object.getOwnPropertyNames(item).map((propName) => (
-        <Text key={propName}>
-          {propName}: {item[propName]}
-        </Text>
-      ))}
+    <View style={styles.item}>
+      <View style={styles.upperContainer}>
+        <View style={styles.avatarContainer}>
+          <Image source={{ uri: item.ownerAvatarUrl }} style={styles.avatar} />
+        </View>
+        <View style={styles.basicInfo}>
+          <View>
+            <Text style={styles.fullName}>{item.fullName}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <View>
+              <Text>{item.description}</Text>
+            </View>
+          </View>
+          <View style={styles.languageWrapper}>
+            <View style={styles.languageButton}>
+              <Text style={styles.language}>{item.language}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.lowerContainer}>
+        <AdditionalInfo label="Stars" value={item.stargazersCount} />
+        <AdditionalInfo label="Forks" value={item.forksCount} />
+        <AdditionalInfo label="Reviews" value={item.reviewCount} />
+        <AdditionalInfo label="Rating" value={item.ratingAverage} />
+      </View>
     </View>
   );
 };
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,19 +4,27 @@ import RepositoryItem from "./RepositoryItem";
 import repositories from "../data/repositories";
 
 const styles = StyleSheet.create({
-  separator: {
-    height: 10,
-  },
+  flatList: { flexShrink: 1 },
+  separator: { height: 10 },
+  header: { height: 10 },
+  footer: { height: 10 },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
+
+const ListHeader = () => <View style={styles.header} />;
+
+const ListFooter = () => <View style={styles.footer} />;
 
 const RepositoryList = () => {
   return (
     <FlatList
       data={repositories}
       renderItem={RepositoryItem}
+      style={styles.flatList}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={ListHeader}
+      ListFooterComponent={ListFooter}
     />
   );
 };
