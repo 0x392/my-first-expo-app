94afd7cb8e0d55996f972f8c0e3c6dad6ba45ffa
Ryan <70819971+0x392@users.noreply.github.com>
2020-10-28 07:00:20 +0800

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,33 @@
 const theme = {
   colors: {
-    textPrimary: "#24292e",
-    textSecondary: "#586069",
-    primary: "#0366d6",
+    bright: {
+      1: "#D8DEE9",
+      2: "#E5E9F0",
+      3: "#ECEFF4",
+      white: "#FFFFFF",
+    },
+    dark: {
+      1: "#4C566A",
+      2: "#434C5E",
+      3: "#3B4252",
+      4: "#2E3440",
+    },
+    frost: {
+      1: "#8FBCBB",
+      2: "#88C0D0",
+      3: "#81A1C1",
+      4: "#5E81AC",
+    },
+    aurora: {
+      1: "#BF616A",
+      2: "#D08770",
+      3: "#EBCB8B",
+      4: "#A3BE8C",
+      5: "#B48EAD",
+    },
   },
   fontSizes: {
-    body: 14,
-    subheading: 16,
-  },
-  fonts: {
-    main: "System",
-  },
-  fontWeights: {
-    normal: "400",
-    bold: "700",
+    default: 16
   },
 };
 
